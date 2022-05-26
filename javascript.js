d4e6b6090ec80a0aeebc4500f700e1064d22dbb8
Cinnlight <cinnlight@gmail.com>
2022-05-26 15:49:20 -0600

    Add additional styling, add click-tracking

--- javascript.js
+++ javascript.js
@@ -1,16 +1,26 @@
 // ----- HTML Selectors ----- //
 
 let buttons = document.querySelectorAll("button");
-let gridContainer = document.querySelector("#gridContainer")
+let gridContainer = document.querySelector("#gridContainer");
+let gridSquare = document.querySelectorAll(".gridSquare");
 
 // ----- Interactive button-click scripting ----- //
 
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
+
+        // Reset canvas to original size & clear canvas
+        if (button.id === "resetCanvas") {
+            if (confirm("Are you sure? This will delete all progress.") == true) {
+                gridSize = 32;
+                removeChildren(gridContainer);
+                makeGrid(gridSize);
+            }
+        }
         
         // Resize the canvas to user-input size
         if (button.id === "resizeCanvas") {
-            gridSize = prompt("Please enter a size between 1-100.");
+            gridSize = prompt("Please enter a size between 1-100.\nThis will delete all progress.");
             if (gridSize <= 100 && gridSize > 0) {
                 removeChildren(gridContainer);
                 makeGrid(gridSize);
@@ -23,6 +33,31 @@ buttons.forEach((button) => {
 
     });
 });
+
+// ----- Mouse up/down scripting to track mouse state ----- //
+
+let mouseDown = 0;
+document.body.addEventListener('mousedown', (event) => {
+    ++mouseDown;
+    console.log('click!')
+});
+
+document.body.addEventListener('mouseup', (event) => {
+    --mouseDown;
+    console.log('un-click!')
+});
+
+if (mouseDown > 1 || mouseDown < 0) {
+    mouseDown = 0;
+    console.log('reset')
+}
+
+if (mouseDown == 1) {
+    document.gridSquare.addEventListener('mouseover', (event) => {
+        gridSquare.target.style.background = black;
+        console.log('draw!')
+    });
+}
 
 // ----- Function to: ----- //
 // Accept user input
